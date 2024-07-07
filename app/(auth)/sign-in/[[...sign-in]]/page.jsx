ff0c1e861c54bb5368250c504065aa1ccf143f7a
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-07-08 03:12:52 +0530

    changed signiin page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -31,12 +31,11 @@ export default function Page() {
             </a>
 
             <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to FinanceMate
             </h2>
 
             <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
+              Track Your Expenses , Make Budgets and Take Control of Your Money
             </p>
           </div>
         </section>
